9f2b63e693bba3327f1a8a4fd28263cbbdc6ce91
Taechawat Konkaew <luangta_5@hotmail.com>
2020-12-02 22:59:21 +0700

    Add day 2 solutions

---
+++ aoc_2020/d02_p01.py
@@ -0,0 +1,27 @@
+from inputs.d02 import PASSWORDS as passwords
+
+# passwords = """
+#     1-3 a: abcde
+#     1-3 b: cdefg
+#     2-9 c: ccccccccc
+# """
+passwords = [line.strip() for line in passwords.splitlines(False) if line.strip() != ""]
+
+
+def is_valid(lowest, highest, character, password):
+    num_occurrences = password.count(character)
+
+    return num_occurrences >= lowest and num_occurrences <= highest
+
+
+valid_count = 0
+for line in passwords:
+    _policy, password = line.split(":")
+    _range, character = _policy.split(" ")
+    lowest, highest = _range.split("-")
+
+    valid_count += is_valid(
+        int(lowest), int(highest), character.strip(), password.strip()
+    )
+
+print(valid_count)
---
+++ aoc_2020/d02_p02.py
@@ -0,0 +1,25 @@
+from inputs.d02 import PASSWORDS as passwords
+
+passwords = [line.strip() for line in passwords.splitlines(False) if line.strip() != ""]
+
+
+def is_valid(first_position, second_position, character, password):
+    is_first_char_correct = password[first_position - 1] == character
+    is_second_char_correct = password[second_position - 1] == character
+
+    return (
+        is_first_char_correct + is_second_char_correct == 1
+    )  # Exactly one of these positions must contain the given letter
+
+
+valid_count = 0
+for line in passwords:
+    _policy, password = line.split(":")
+    _range, character = _policy.split(" ")
+    lowest, highest = _range.split("-")
+
+    valid_count += is_valid(
+        int(lowest), int(highest), character.strip(), password.strip()
+    )
+
+print(valid_count)
