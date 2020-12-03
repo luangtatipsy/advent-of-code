48207a2bd33cddfd2038f0d20a25252b2d0ef858
Taechawat Konkaew <luangta_5@hotmail.com>
2020-12-03 21:23:09 +0700

    Add test for day_2

--- 2020/day_2/part_1.py
+++ 2020/day_2/part_1.py
@@ -1,21 +1,27 @@
-with open("input.txt") as f:
-    passwords = [line.strip() for line in f.read().splitlines(False) if line.strip() != ""]
-
-
 def is_valid(lowest, highest, character, password):
     num_occurrences = password.count(character)
 
     return num_occurrences >= lowest and num_occurrences <= highest
 
-
-valid_count = 0
-for line in passwords:
+def extract_policy(line):
     _policy, password = line.split(":")
     _range, character = _policy.split(" ")
     lowest, highest = _range.split("-")
 
-    valid_count += is_valid(
-        int(lowest), int(highest), character.strip(), password.strip()
-    )
+    return int(lowest), int(highest), character.strip(), password.strip()
 
-print(valid_count)
+
+def main(passwords):
+    valid_count = 0
+    for line in passwords:
+        lowest, highest, character, password = extract_policy(line)
+        valid_count += is_valid(lowest, highest, character, password)
+
+    return valid_count
+
+if __name__ == "__main__":    
+    with open("input.txt") as f:
+        passwords = [line.strip() for line in f.read().splitlines(False) if line.strip() != ""]
+
+    valid_count = main(passwords)
+    print(valid_count)
--- 2020/day_2/part_2.py
+++ 2020/day_2/part_2.py
@@ -1,7 +1,3 @@
-with open("input.txt") as f:
-    passwords = [line.strip() for line in f.read().splitlines(False) if line.strip() != ""]
-
-
 def is_valid(first_position, second_position, character, password):
     is_first_char_correct = password[first_position - 1] == character
     is_second_char_correct = password[second_position - 1] == character
@@ -10,15 +6,25 @@ def is_valid(first_position, second_position, character, password):
         is_first_char_correct + is_second_char_correct == 1
     )  # Exactly one of these positions must contain the given letter
 
-
-valid_count = 0
-for line in passwords:
+def extract_policy(line):
     _policy, password = line.split(":")
     _range, character = _policy.split(" ")
     lowest, highest = _range.split("-")
 
-    valid_count += is_valid(
-        int(lowest), int(highest), character.strip(), password.strip()
-    )
+    return int(lowest), int(highest), character.strip(), password.strip()
 
-print(valid_count)
+
+def main(passwords):
+    valid_count = 0
+    for line in passwords:
+        lowest, highest, character, password = extract_policy(line)
+        valid_count += is_valid(lowest, highest, character, password)
+
+    return valid_count
+
+if __name__ == "__main__":    
+    with open("input.txt") as f:
+        passwords = [line.strip() for line in f.read().splitlines(False) if line.strip() != ""]
+
+    valid_count = main(passwords)
+    print(valid_count)
