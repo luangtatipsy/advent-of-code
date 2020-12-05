ee09755ec39335a6ab7d73f747c99f213e308826
Taechawat Konkaew <luangta_5@hotmail.com>
2020-12-06 01:31:20 +0700

    Add more test case for Day 5

--- 2020/tests/test_day_5.py
+++ 2020/tests/test_day_5.py
@@ -1,24 +1,31 @@
-from day_5.part_1 import extract_seat_id as part_1_solution
+from day_5.part_1 import extract_seat_id
+from day_5.part_1 import main as part_1_solution
 
 # Part 2 solution does not change core program logic then, it's not necessary to test it
 
 
-def test_part_1_solution_1():
-    boarding_pass = "FBFBBFFRLR"
-    assert part_1_solution(boarding_pass) == 357
+boarding_passes = ["FBFBBFFRLR", "BFFFBBFRRR", "FFFBBBFRRR", "BBFFBBFRLL"]
 
 
-def test_part_1_solution_2():
-    boarding_pass = "BFFFBBFRRR"
-    assert part_1_solution(boarding_pass) == 567
+def test_extract_seat_id_1():
+    boarding_pass = boarding_passes[0]
+    assert extract_seat_id(boarding_pass) == 357
 
 
-def test_part_1_solution_3():
-    boarding_pass = "FFFBBBFRRR"
-    assert part_1_solution(boarding_pass) == 119
+def test_extract_seat_id_2():
+    boarding_pass = boarding_passes[1]
+    assert extract_seat_id(boarding_pass) == 567
 
 
-def test_part_1_solution_4():
-    boarding_pass = "BBFFBBFRLL"
-    assert part_1_solution(boarding_pass) == 820
+def test_extract_seat_id_3():
+    boarding_pass = boarding_passes[2]
+    assert extract_seat_id(boarding_pass) == 119
 
+
+def test_extract_seat_id_4():
+    boarding_pass = boarding_passes[3]
+    assert extract_seat_id(boarding_pass) == 820
+
+
+def test_part_1_solution():
+    assert part_1_solution(boarding_passes) == 820
